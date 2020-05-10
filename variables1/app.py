350b165ab301afbfec347eac06e2758f797f49fe
GaneshA5 <alagarganesh@gmail.com>
2020-05-10 16:21:51 +0530

    Variables in flask

---
+++ variables1/app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+import datetime
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+	now=datetime.datetime.now()
+	newyear= now.month == 1 and now.day == 1
+	return render_template("index.html",newyear=newyear)
+
